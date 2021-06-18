424ead570daa5029bb5480d9ba40b2c5794ffeda
E-jarod <ejilane.jarod@gmail.com>
2021-06-18 22:26:29 +0200

    removed test, added flutter_bloc + path_provider

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'School marks',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MyHomePage(title: 'School marks'),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
